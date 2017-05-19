fa1b099c5eea28de511e1794a7457558443e432b
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-19 00:16:24 -0400

    updated

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -7,21 +7,27 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+import { ChartModule, ChartService } from 'ngnvd3';
+import { DemoComponent } from './components/demo/demo.component';
+
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [AppComponent],
     declarations: [
         AppComponent,
+        DemoComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
         HomeComponent
     ],
     imports: [
+        ChartModule,
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
+             { path: 'demo', component: DemoComponent },
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])

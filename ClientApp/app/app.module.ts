969ce3bbbd7c22023ca5301e54c6b6d279755878
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-24 15:47:39 -0400

    updated

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,14 +1,18 @@
+import { ContentComponent } from './containers/content/content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 
 import { ChartModule, ChartService } from 'ngnvd3';
 import { AppComponent } from './containers/app/app.component';
+import { NavigationComponent } from './containers/navigation/navigation.component';
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
+        NavigationComponent,
+        ContentComponent
     ],
     imports: [
         ChartModule,

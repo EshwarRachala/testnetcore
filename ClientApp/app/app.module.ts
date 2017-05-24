82463c7a9c5517c1551704b419932c7ce2a46bef
eshwar rachala <eshwar.appdev@gmail.com>
2017-05-24 14:43:40 -0400

    updated

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,36 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
-import { AppComponent } from './components/app/app.component'
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 import { ChartModule, ChartService } from 'ngnvd3';
-import { DemoComponent } from './components/demo/demo.component';
+import { AppComponent } from './containers/app/app.component';
 
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [
         AppComponent,
-        DemoComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
     ],
     imports: [
         ChartModule,
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-             { path: 'demo', component: DemoComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
+        UniversalModule, 
     ]
 })
 export class AppModule {
